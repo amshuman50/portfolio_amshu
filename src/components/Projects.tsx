da0f01421e9caeb4585897d77e18e465f59fe1da
amshuman50 <amshuman789@gmail.com>
2026-01-18 11:00:09 +0545

    commented out

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,10 +2,10 @@ import { ProjectsSection } from "./sections/ProjectsSection";
 import { projects } from "@/data/projects";
 
 export const Projects = () => {
-  console.log("Projects component rendered");
-  console.log("Projects data type:", typeof projects);
-  console.log("Is projects an array?", Array.isArray(projects));
-  console.log("Projects data:", projects);
+  // console.log("Projects component rendered");
+  // console.log("Projects data type:", typeof projects);
+  // console.log("Is projects an array?", Array.isArray(projects));
+  // console.log("Projects data:", projects);
   
   if (!projects || !Array.isArray(projects)) {
     console.error("Projects data is not available or not an array");
