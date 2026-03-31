e4308b9a82e6073226d1c82a495bf9ed12e5f681
amshuman50 <amshuman789@gmail.com>
2026-03-31 10:09:36 +0545

    Added Images in Project Section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,31 +5,36 @@ export interface Project {
   technologies: string[];
   githubUrl?: string;
   liveUrl?: string;
+  image?: string;
 }
 
 export const projects: Project[] = [
   {
     id: 1,
-    title: "Simple Chat System",
-    description: "A simple chat system You can join by using your name in different browser tab.[Open your browser tabs side by side]",
-    technologies: ["React", "Next.js", "Socket.io", "TailwindCSS"],
-    githubUrl: "https://github.com/amshuman50/Simple_chat",
-    liveUrl: "https://simple-chat-one-sigma.vercel.app/"
+    title: 'Text Utilities',
+    description: 'A text utility website for text manipulation.',
+    technologies: ['TypeScript', 'Next.js', 'Bootstrap'],
+    githubUrl: 'https://github.com/amshuman50/text_utilities',
+    liveUrl: 'https://amshuman50.github.io/text_utilities/',
+    image: '/projects/text_utils.png',
   },
   {
     id: 2,
-    title: "Text Utilities",
-    description: "A text utility website for text manipulation.",
-    technologies: ["TypeScript", "Next.js", "Bootstrap"],
-    githubUrl: "https://github.com/amshuman50/text_utilities",
-    liveUrl: "https://amshuman50.github.io/text_utilities/"
+    title: 'Simple Chat System',
+    description:
+      'A simple chat system You can join by using your name in different browser tab.[Open your browser tabs side by side]',
+    technologies: ['React', 'Next.js', 'Socket.io', 'TailwindCSS'],
+    githubUrl: 'https://github.com/amshuman50/Simple_chat',
+    liveUrl: 'https://simple-chat-one-sigma.vercel.app/',
+    image: '/projects/chat.png',
   },
   {
     id: 3,
-    title: "Data Collection Form",
-    description: "Data Collection Form for a survey.",
-    technologies: ["React", "Next.js", "tailwind css"],
-    githubUrl: "https://github.com/amshuman50/farmer_form",
-    liveUrl: "https://farmer-form.vercel.app/"
-  }
-]; 
+    title: 'Data Collection Form',
+    description: 'Data Collection Form for a survey.',
+    technologies: ['React', 'Next.js', 'Tailwind CSS'],
+    githubUrl: 'https://github.com/amshuman50/farmer_form',
+    liveUrl: 'https://farmer-form.vercel.app/',
+    image: '/projects/data_collection.png',
+  },
+];
